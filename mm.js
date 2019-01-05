1d213de97c3aac3d365fd4f836523a6a1f6351e0
kenh <1228337123hrg@gmail.com>
2019-01-05 15:51:38 +0800

    Update mm.js

--- mm.js
+++ mm.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
 var request = require("request");
-var src = "http://img1.mm131.me/pic/2229/7.jpg";
+var src = "http://example.com";
 var writeStream = fs.createWriteStream('image007.png');
 
 var options = { method: 'GET',
-    url: 'http://img1.mm131.me/pic/2229/7.jpg',
+    url: 'http://example.com',
     headers:
         { 'Postman-Token': '2004cd5b-0c68-444b-9704-ce07489060b5',
             'cache-control': 'no-cache',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6788.400 QQBrowser/10.3.2767.400',
             'Accept-Language': 'zh-CN,zh;q=0.9',
-            Referer: 'http://www.mm131.com/xinggan/2229_7.html',
+            Referer: 'http://example.com',
             'Accept-Encoding': 'gzip, deflate',
             Connection: 'keep-alive',
             Accept: 'image/webp,image/apng,image/*,*/*;q=0.8' } };
